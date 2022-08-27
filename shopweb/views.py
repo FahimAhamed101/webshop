1d6aa6d771eeb7bcd3e940a9ca0aad249c4524c6
tom <tomalahamed0@gmail.com>
2022-08-27 11:19:03 -0700

    fixed

--- shopweb/views.py
+++ shopweb/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def Products(request):
+    return render(request, 'products.html')
+def cart(request):
+    return render(request, 'cart.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
