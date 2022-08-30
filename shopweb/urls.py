2fba8b1e57ea25799785f2674ef7089ba58e75ce
tom <tomalahamed0@gmail.com>
2022-08-30 05:49:02 -0700

    adjdjl

--- shopweb/urls.py
+++ shopweb/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
           path('about', views.about,name='about'),
            path('contact', views.contact,name='contact'),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#
