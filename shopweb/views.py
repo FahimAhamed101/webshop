305800b40c13836a400dfff55b5a2eae90a16909
Fahim ahamed <54832397+FahimAhamed101@users.noreply.github.com>
2023-01-18 16:16:05 +0600

    Update views.py

--- shopweb/views.py
+++ shopweb/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from products.models import Product
 def home(request):
-    return render(request, 'index.html')
+     products = Product.objects.all().filter(is_available=True).order_by('created_date')
+
+
+
+    context = {
+        'products': products,
+       
+    }
+    return render(request, 'index.html',context)
 
 def Products(request):
     return render(request, 'products.html')
