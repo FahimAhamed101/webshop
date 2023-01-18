35682b9ab1956cf5dcc544ce3cc6d253203f577f
Fahim ahamed <54832397+FahimAhamed101@users.noreply.github.com>
2023-01-18 16:18:45 +0600

    Update views.py

--- shopweb/views.py
+++ shopweb/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from products.models import Product
 def home(request):
-     products = Product.objects.all().filter(is_available=True).order_by('created_date')
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')
 
 
 
